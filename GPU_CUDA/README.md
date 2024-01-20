e567059f2ea0704ca946eb7d2f02a41964423612
Navya <122176358+navyaswe@users.noreply.github.com>
2024-01-19 19:44:00 -0800

    Add files via upload

---
+++ GPU_CUDA/README.md
@@ -0,0 +1,74 @@
+# Digit Recognizer
+
+Handwritten digit recognition ML program written from scratch with CUDA for parallel computations which uses an FNN to predict 
+the digit entered. The number of hidden layers and the number of hidden units can be adjusted to any values when running the 
+program by specifying options.
+
+### Features
+There are 4 different modes in which the program can run. How to select a mode is given in the "Use" section of this file.
+
+##### 1. The Training Mode. 
+The training mode allows user to alter the hyperparameters like learning rate, decay factor, epochs, batch size, number of hidden layers and neurons to any value that they wish. How these options are set is given in the
+options_help.txt file present in the /docs folder.
+
+##### 2. The Complete Test Mode
+This tests the model on all of the 10k images of MNIST database in about two seconds.
+
+
+##### 3. The Single Test Mode
+This test launches an OpenGL graphics window along with the main program. When the user clicks on the green arrow button on the 
+window, a new image from the database is loaded on the window and is passed to the FNN and the FNN's guess is shown on the
+terminal window. Pressing the red X button will cause the program to terminate.
+
+
+##### 4. The Draw Test Mode
+This feature of the program is not entirely perfect yet but still it gives quite acceptable results. Using this mode, the user can draw on an OpenGL window which will be launched along with the program. And pass
+the current frame state to the FNN by pressing the green arrow button and let the FNN make its best guess. Pressing the red X
+button will cause the window frame to get cleared for drawing the next test digit.
+
+### Requirements
+	1. Linux OS
+	2. OpenGL library*       (mesa-utils and freeglut3-dev)
+	3. g++*
+	4. Nvidia GPU 	   
+	5. nvcc*  
+	
+	 *needed only for building the project again.
+
+If you don't have OpenGL or nvcc you can still use this program using the pre-built binary file provided in the /bin folder.
+However, you will still need an Nvidia GPU to run it.
+
+### Build
+
+1. Right click the build.sh file. Click on "Properties". Go to "Permissions" tab and make sure the checkbox reading "Allow
+executing file as a program" is checked.
+
+2. Right click the empty_build.sh file. Click on "Properties". Go to "Permissions" tab and make sure the checkbox reading "Allow
+executing file as a program" is checked.
+
+3. In the terminal (navigated to the project directory), type 
+	```
+	./build.sh
+	```
+        
+to build the project. You will find the produced binary in the /bin folder.
+
+4. To empty the /bin folder type in the terminal (navigated to the project directory) 
+	```
+	./empty_build.sh
+	```
+	
+### Use
+
+Launch a terminal naviagted to the /bin folder in this project. To launch the program, enter
+```
+./recog [options]
+```
+
+Check "options_help.txt" file in the /docs folder of the project for the list of options and their meanings. Or just type 
+```
+./recog
+```
+One pre-trained feed-forward neural network is provided in the /saved_FNN folder of 2 hidden layers of 200 and 60 neurons having an accuracy of
+95.840004% named "mnist.fnn". It is the default choice of the program so if you don't specify the name of the FNN to be used,
+that is the one which will be used.
---
+++ GPU_CUDA/src/FNN.hpp
@@ -0,0 +1,323 @@
+/**
+ * Supports a maximum of 22 layers (including input and output layers).
+ * 
+ * NOTE:- The FNN does not save the updated weights and biases automatically after every batch is done
+ * It leaves that to the main caller funciton. You have to call the FNN::save() function whenever you
+ * wish to write the updated weights and biases. This will allow you to control what value is being written
+ * so that weights and biases changes are not written when, for example, say the gradient explodes.
+ */
+
+#ifndef FNN_HPP
+#define FNN_HPP
+#endif
+
+#include "cuda.hpp"
+#include <vector>
+#include <string>
+#include <time.h>
+#include <fstream>
+#include <math.h>
+#include <stack>
+
+using namespace std;
+
+
+
+class FNN {
+private:
+	int number_of_layers;
+	vector<int> layers;
+	int epochs, batch_size, epoch_dropout, samples_done, batches_done, number_of_samples;
+	float learning_rate, decay_rate;
+	int weights, biases;
+	string file_name;
+	ifstream file_in; ofstream file_out;
+	float cost, avg_cost;
+	bool cuda_gpu_found;
+
+	//NN parameters. All are declared in the unified memory of GPU and CPU
+	float *w[21], *b[21], *a[23], *dw[21], *db[21], *da[23], *o;
+
+public:
+	FNN(void);
+	~FNN(void);
+	void init(vector<int>, int, int, int, float, float, string);
+	bool load(void);
+	void save(void);
+	void xavier_initialize(void);
+	void allocate(void);
+	string get_layers_sizes_string(void);
+	int get_layers(void);
+	float get_loss(void);
+	int output(void);
+	void forward_prop(vector<float>&);
+	void backprop(int, int);
+	void backprop(float*, int);
+	void print_last_layer(void);
+	string int_to_string(int);
+	bool good(void);
+	float cross_entropy(float y, float y_cap);
+	bool NaN();
+	void set_number_of_samples(int);
+};
+
+FNN::FNN() {
+	cuda_gpu_found = cuda_init();
+	cost = avg_cost = 0; samples_done = batches_done = 0;
+}
+
+FNN::~FNN() {
+	for (int i = 0; i < number_of_layers - 1; i++) {
+		cuda_delete(&w[i]);
+		cuda_delete(&dw[i]);
+		cuda_delete(&b[i]);
+		cuda_delete(&db[i]);
+		cuda_delete(&a[i]);
+		cuda_delete(&da[i]);
+	}
+	for (int i = number_of_layers - 1; i <= number_of_layers; i++) {
+		cuda_delete(&a[i]);
+		cuda_delete(&da[i]);
+	}
+	cuda_delete(&o);
+	cuda_destroy();
+}
+
+bool FNN::good() {
+	return cuda_gpu_found;
+}
+
+string FNN::int_to_string(int i) {
+	string res = "";
+	stack<char> temp;
+	while (i) {
+		temp.push((char)(i % 10) + '0');
+		i /= 10;
+	}
+	while (!temp.empty()) {
+		res = res + temp.top();
+		temp.pop();
+	}
+	return res;
+}
+
+int FNN::output() {
+	int max = 0;
+	for (int i = 0; i < layers[number_of_layers - 1]; i++) {
+		if (a[number_of_layers][i] > a[number_of_layers][max]) {
+			max = i;
+		}
+	}
+	return max;
+}
+
+float FNN::get_loss() {
+	return avg_cost;
+}
+
+int FNN::get_layers() {
+	return number_of_layers;
+}
+
+string FNN::get_layers_sizes_string() {
+	string output = "";
+	for (int i = 0; i < number_of_layers - 1; i++) {
+		output = output + int_to_string(layers[i]) + ", ";
+	}
+	output = output + int_to_string(layers[number_of_layers - 1]);
+	return output;
+}
+
+void FNN::allocate() {
+	for (int i = 1; i < number_of_layers; i++) {
+		cuda_allocate(&w[i - 1], layers[i] * layers[i - 1]);
+		cuda_allocate(&dw[i - 1], layers[i] * layers[i - 1]);
+	}
+	for (int i = 1; i < number_of_layers; i++) {
+		cuda_allocate(&b[i - 1], layers[i]);
+		cuda_allocate(&db[i - 1], layers[i]);
+	}
+	for (int i = 0; i < number_of_layers; i++) {
+		cuda_allocate(&a[i], layers[i]);
+		cuda_allocate(&da[i], layers[i]);
+	}
+	cuda_allocate(&a[number_of_layers], layers[number_of_layers - 1]);
+	cuda_allocate(&da[number_of_layers], layers[number_of_layers - 1]);
+	cuda_allocate(&o, layers[number_of_layers - 1]);
+}
+
+void FNN::xavier_initialize() {
+	srand(time(NULL));
+	for (int i = 0; i < number_of_layers - 1; i++) {
+		float standard_deviation = sqrt((float)1 / layers[i]);
+		int temp = layers[i] * layers[i + 1];
+		for (int j = 0; j < temp; j++) {
+			int random;
+			do {
+				random = rand() % ((int) (standard_deviation * 200000));
+			} while (random == 0);
+			w[i][j] = random;
+			w[i][j] -= ((int)(standard_deviation * 100000));
+			w[i][j] = ((float)(w[i][j] / 100000));
+		}
+	}
+	for (int i = 0; i < number_of_layers - 1; i++) {
+		for (int j = 0; j < layers[i + 1]; j++) {
+			b[i][j] = 0;	
+		}
+	}
+}
+
+bool FNN::load() {
+	file_in.open(file_name, ios::binary);
+	if (!file_in.good()) {
+		return 0;
+	}
+	file_in.read((char *) &number_of_layers, sizeof(number_of_layers));
+	layers.clear();
+	for (int i = 0; i < number_of_layers; i++) {
+		int temp;
+		file_in.read((char *) &temp, sizeof(temp));
+		layers.push_back(temp);
+	}
+	file_in.read((char *) &epochs, sizeof(epochs));
+	file_in.read((char *) &batch_size, sizeof(batch_size));
+	file_in.read((char *) &epoch_dropout, sizeof(epoch_dropout));
+	file_in.read((char *) &learning_rate, sizeof(learning_rate));
+	file_in.read((char *) &decay_rate, sizeof(decay_rate));
+	allocate();
+	
+	for (int i = 1; i < number_of_layers; i++) {
+		int temp = layers[i] * layers[i - 1];
+		for (int j = 0; j < temp; j++) {
+			file_in.read((char *) &w[i - 1][j], sizeof(w[i - 1][j]));
+		}
+	}
+
+	for (int i = 1; i < number_of_layers; i++) {
+		for (int j = 0; j < layers[i]; j++) {
+			file_in.read((char *) &b[i - 1][j], sizeof(b[i - 1][j]));
+		}
+	}
+	file_in.close();
+	return 1;
+}
+
+void FNN::save() {
+	remove(file_name.c_str());
+	file_out.open(file_name, ios::binary);
+
+	file_out.write((char *) &number_of_layers, sizeof(number_of_layers));
+	for (int i = 0; i < number_of_layers; i++) {
+		int temp = layers[i];
+		file_out.write((char *) &temp, sizeof(temp));
+	}
+	file_out.write((char *) &epochs, sizeof(epochs));
+	file_out.write((char *) &batch_size, sizeof(batch_size));
+	file_out.write((char *) &epoch_dropout, sizeof(epoch_dropout));
+	file_out.write((char *) &learning_rate, sizeof(learning_rate));
+	file_out.write((char *) &decay_rate, sizeof(decay_rate));
+	
+	for (int i = 1; i < number_of_layers; i++) {
+		int temp = layers[i] * layers[i - 1];
+		for (int j = 0; j < temp; j++) {
+			file_out.write((char *) &w[i - 1][j], sizeof(w[i - 1][j]));
+		}
+	}
+
+	for (int i = 1; i < number_of_layers; i++) {
+		for (int j = 0; j < layers[i]; j++) {
+			file_out.write((char *) &b[i - 1][j], sizeof(b[i - 1][j]));
+		}
+	}
+
+	file_out.close();
+}
+
+void FNN::init(vector<int> layers, int epochs, int batch_size, int epoch_dropout, 
+               float learning_rate, float decay_rate, string file_name) {
+	this->file_name = file_name;
+	if (!load()) {
+		this->layers.clear();
+		this->layers = layers;
+		this->number_of_layers = layers.size();
+		this->epochs = epochs;
+		this->batch_size = batch_size;
+		this->epoch_dropout = epoch_dropout;
+		this->learning_rate = learning_rate;
+		this->decay_rate = decay_rate;
+		allocate();
+		xavier_initialize();
+		save();
+	}
+}
+
+void FNN::forward_prop(vector<float> &ip) {
+	for (int i = 0; i < layers[0]; i++) {
+		a[0][i] = ip[i];
+	}
+	for (int i = 1; i < number_of_layers; i++) {
+		cuda_matrix_mul(&a[i], &w[i - 1], &a[i - 1], layers[i], layers[i - 1], 1);
+		cuda_matrix_add(&a[i], &a[i], &b[i - 1], layers[i], 1);
+		cuda_ReLU(&a[i], &a[i], layers[i]);
+	}
+	cuda_softmax(&a[number_of_layers], &a[number_of_layers - 1], layers[number_of_layers - 1]);
+}
+
+void FNN::backprop(int op, int e) {
+	float *output = new float[layers[number_of_layers - 1]];
+	for (int i = 0; i < layers[number_of_layers - 1]; i++) {
+		output[i] = 0;
+	}
+	output[op] = 1;
+	backprop(output, e);
+}
+
+void FNN::print_last_layer() {
+	printf("Last softmax layer looks like this:-\n");
+	for (int i = 0; i < layers[number_of_layers - 1]; i++) {
+		printf("%f\n", a[number_of_layers][i]);
+	}
+	printf("\n");
+}
+
+float FNN::cross_entropy(float y_cap, float y) {
+	return -((y * log(y_cap)) + ((1 - y) * log(1 - y_cap)));
+}
+
+void FNN::backprop(float *op, int e) {
+	cuda_copy_to_device(&o, &op, layers[number_of_layers - 1]);
+	for (int i = 0; i < layers[number_of_layers - 1]; i++) {
+		cost += cross_entropy(a[number_of_layers][i], op[i]);
+	}
+	cuda_cross_entropy_prime(&da[number_of_layers], &a[number_of_layers], &o, layers[number_of_layers - 1]);
+	cuda_softmax_prime(&da[number_of_layers - 1], &a[number_of_layers], 
+                        &da[number_of_layers], layers[number_of_layers - 1]);
+	for (int i = number_of_layers - 1; i > 0; i--) {
+		cuda_ReLU_prime_biases(&db[i - 1], &a[i], &da[i], layers[i]);
+		cuda_ReLU_prime_others(&da[i - 1], &dw[i - 1], &w[i - 1], &a[i - 1], &a[i], &da[i], layers[i - 1], layers[i]);
+	}
+	samples_done++;
+	if (samples_done == batch_size) {
+		samples_done = 0;
+		float lr = learning_rate * pow(decay_rate, (floor(e / epoch_dropout)));
+		for (int i = 0; i < number_of_layers - 1; i++) {
+			cuda_gradient_descent_step(&w[i], &dw[i], lr, batch_size, (layers[i] * layers[i + 1]));
+			cuda_gradient_descent_step(&b[i], &db[i], lr, batch_size, layers[i + 1]);
+		}
+		batches_done++;
+		if (batches_done * batch_size == number_of_samples) {
+			avg_cost = cost / (batch_size * batches_done * layers[number_of_layers - 1]);
+			batches_done = 0;
+			cost = 0;
+		}
+	}
+}
+
+void FNN::set_number_of_samples(int n) {
+	number_of_samples = n;
+}
+
+#ifndef WINDOW_HPP
+#include "window.hpp"
+#endif
